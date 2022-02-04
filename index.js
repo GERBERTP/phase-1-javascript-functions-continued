ad0a13d9cad51b7f18886e9b0bb7e1a9b6c117a8
Gerbert Pesquera <gerbert.pesquera@gmail.com>
2022-02-04 17:21:55 -0500

    commit tree 4940acc79723934ee405cee9b72792899e3ac63a

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+function saturdayFun(fun){
+    if(fun === "bathe my dog"){
+        return `This Saturday, I want to ${fun}!`
+    } else {
+        return "This Saturday, I want to roller-skate!"
+    }
+}
+
+function mondayWork(taco){
+    if (taco === "work from home"){
+        return "This Monday, I will work from home."
+    } else{
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(string = "*"){
+
+    return function (adjective = "special"){
+
+            console.log(`You are ${string}${adjective}${string}!`);
+        return `You are ${string}${adjective}${string}!`
+   }
+};
